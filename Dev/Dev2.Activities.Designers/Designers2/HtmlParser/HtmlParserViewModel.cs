9e4d527f7303725cd9112f840e24f3f13aee403c
Simonas Starkus <simas@jip.lt>
2016-11-10 00:04:11 +0200

    added demo initial html parser version

---
+++ Dev/Dev2.Activities.Designers/Designers2/HtmlParser/HtmlParserViewModel.cs
@@ -0,0 +1,56 @@
+﻿using Dev2.Activities.Designers2.Core;
+using System;
+using System.Activities.Presentation.Model;
+
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dev2.Activities.Designers2.HtmlParser
+{
+    public class HtmlParserViewModel: ActivityDesignerViewModel, INotifyPropertyChanged
+    {
+        public HtmlParserViewModel(ModelItem modelItem) : base(modelItem)
+        {
+
+        }
+        public string Left
+        {
+            get { return GetProperty<string>(); }
+            set
+            {
+                SetProperty(value);
+                OnPropertyChanged("Operation");
+            }
+        }
+        public string Right
+        {
+            get { return GetProperty<string>(); }
+            set
+            {
+                SetProperty(value);
+                OnPropertyChanged("Operation");
+            }
+        }
+        public override void Validate()
+        {
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+       
+        protected void OnPropertyChanged(string propertyName = null)
+        {
+            var handler = PropertyChanged;
+            if (handler != null)
+            {
+                handler(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+        public override void UpdateHelpDescriptor(string helpText)
+        {
+            //throw new NotImplementedException();
+        }
+    }
+}
---
+++ Dev/Dev2.Activities/Activities/DsfHtmlParserActivity.cs
@@ -0,0 +1,69 @@
+﻿
+
+using System;
+using System.Activities;
+using System.Activities.Presentation.Model;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using Dev2.Activities.Debug;
+using Dev2.Common.Interfaces.Diagnostics.Debug;
+using Dev2.Common.Interfaces.Toolbox;
+using Dev2.Data;
+using Dev2.Data.Parsers;
+using Dev2.Data.Util;
+using Dev2.DataList.Contract;
+using Dev2.Diagnostics;
+using Dev2.Interfaces;
+using Unlimited.Applications.BusinessDesignStudio.Activities;
+using Warewolf.Core;
+using Warewolf.Storage;
+
+
+namespace Dev2.Activities
+{
+    [ToolDescriptorInfo("Html parser", "Html", ToolType.Native, "F578E19B-A877-4216-B70B-E951D5964D57", "Dev2.Acitivities", "1.0.0.0", "Legacy", "Utility", "/Warewolf.Studio.Themes.Luna;component/Images.xaml", "Tool_Utility_Xpath_Tags")]
+
+    public class DsfHtmlParserActivity : DsfActivityAbstract<string>, ICollectionActivity
+    {
+        public void AddListToCollection(IList<string> listToAdd, bool overwrite, ModelItem modelItem)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int GetCollectionCount()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override IList<DsfForEachItem> GetForEachInputs()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override IList<DsfForEachItem> GetForEachOutputs()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void UpdateForEachInputs(IList<Tuple<string, string>> updates)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void UpdateForEachOutputs(IList<Tuple<string, string>> updates)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void ExecuteTool(IDSFDataObject dataObject, int update)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void OnExecute(NativeActivityContext context)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
